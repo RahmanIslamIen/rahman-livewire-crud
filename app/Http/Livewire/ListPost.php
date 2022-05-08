e91ba95ed437532bb7c0b6f5dbe4c47c88ed4b7b
Rahman Islam <54945042+RahmanIslamIen@users.noreply.github.com>
2022-05-08 11:25:53 +0700

    final crud

--- app/Http/Livewire/ListPost.php
+++ app/Http/Livewire/ListPost.php
@@ -7,6 +7,9 @@ use Livewire\Component;
 
 class ListPost extends Component
 {
+    public $updateStateId = 0;
+    public $body = 0;
+
     protected $listeners = [
         'postCreated' => '$refresh'
     ];
@@ -16,5 +19,26 @@ class ListPost extends Component
         return view('livewire.list-post', [
             'posts' => Post::latest()->get()
         ]);
+    }
+
+    public function showUpdateForm($postId){
+        $post = Post::find($postId);
+        $this->body = $post->body;
+        $this->updateStateId = $postId;
+    }
+
+    public function update($postId)
+    {
+        $post = Post::find($postId);
+        $post->body = $this->body;
+        $post->save();
+
+        $this->updateStateId = 0;
+    }
+
+    public function delete($postId)
+    {
+        $post = Post::find($postId);
+        $post->delete();
     }
 }
--- resources/views/livewire/list-post.blade.php
+++ resources/views/livewire/list-post.blade.php
@@ -1,12 +1,29 @@
 <div class="p-4 flex-col">
+    {{ $updateStateId }}
     @foreach ($posts as $post)
         <div class="p-4 my-4 bg-white shadow-xl rounded-md">
             <div class="block">
                 <span class="text-xl font-bold">{{ $post->user->name }}</span>
                 <span class="text-gray-600">{{ $post->created_at->diffForHumans() }}</span>
+                <button wire:click="showUpdateForm({{ $post->id }})"
+                    class="p-2 btn btn-success rounded-md">Edit</button>
+                <button onclick="return confirm('benar mau dihapus?') || event.stopImmediatePropagation()"
+                    wire:click="delete({{ $post->id }})" class="p-2 btn btn-danger rounded-md">Delete</button>
             </div>
             <div>
-                <span>{{ $post->body }}</span>
+                @if ($updateStateId != $post->id)
+                    <span>{{ $post->body }}</span>
+                @endif
+
+                @if ($updateStateId === $post->id)
+                    <textarea wire:model="body" name="body"
+                        class="appearance-none rounded-none relative block w-full px-3 py-2 
+                        border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md 
+                        focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 
+                        sm:text-sm"
+                        rows="3"></textarea>
+                    <button wire:click="update({{ $post->id }})" class="m-2 btn btn-primary px-4 py-2">Save</button>
+                @endif
             </div>
         </div>
     @endforeach
